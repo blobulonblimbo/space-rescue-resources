cfa832ddaa5b0f2328af354309c740f687afcc7f
RickyBodaj <139197735+blobulonblimbo@users.noreply.github.com>
2023-07-25 14:16:34 +1000

    DRIFT

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,5 +1,6 @@
-from GameFrame import RoomObject
+from GameFrame import Globals, RoomObject
 import pygame
+
 class Ship(RoomObject):
     """
     A class for the player's avitar (the Ship)
@@ -24,6 +25,39 @@ class Ship(RoomObject):
         """
         
         if key[pygame.K_w]:
-            self.y_speed = -10
-        elif key[pygame.K_s]:
-            self.y_speed = 10
+            if self.y_speed > -5:
+                self.y_speed -= 0.1
+        else:
+            if self.y_speed < 0:
+                self.y_speed += 0.1
+        if key[pygame.K_s]:
+            if self.y_speed < 5:
+                self.y_speed += 0.1
+        else:
+            if self.y_speed > 0:
+                self.y_speed -= 0.1
+        if key[pygame.K_a]:
+            if self.x_speed > -5:
+                self.x_speed -= 0.1
+        else:
+            if self.x_speed < 0:
+                self.x_speed += 0.1
+        if key[pygame.K_d]:
+            if self.y_speed < 5:
+                self.x_speed += 0.1
+        else:
+            if self.x_speed > 0:
+                self.x_speed -= 0.1
+        
+        
+        
+    def keep_in_room(self):
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.height> Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+    def step(self):
+        """
+        Determine what happens to the Ship on each click of the game clock
+        """
+        self.keep_in_room()
