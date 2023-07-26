aeb8c963e6c61bd52771d39a8750c9a4f31d20cc
RickyBodaj <139197735+blobulonblimbo@users.noreply.github.com>
2023-07-26 10:20:32 +1000

    Update Zork.py

--- Objects/Zork.py
+++ Objects/Zork.py
@@ -15,7 +15,8 @@ class Zork(RoomObject):
         # set image
         image = self.load_image("Zork.png")
         self.set_image(image,135,165)
-        self.y_speed = random.choice([-1,1])
+        self.y_speed = random.choice([-5,5])
+        self.x_speed = random.choice([-3,3])
 
     def keep_in_room(self):
         """
@@ -23,4 +24,16 @@ class Zork(RoomObject):
         """
         if self.y < 0 or self.y > Globals.SCREEN_HEIGHT - self.height:
             self.y_speed *= -1
+        if self.x_speed > 0 or self.x_speed < 0:
+            if self.x_speed > 0:
+                self.x_speed -= 0.1
+            elif self.x_speed < 0:
+                self.x_speed += 0.1
+            
+            
+    def step(self):
+        """
+        Determine what happens to the Dragon on each tick of the game clock
+        """
+        self.keep_in_room()
             
